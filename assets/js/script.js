6900fdc519c1ab687994d45fb826423dc4bf11ae
JINHEE LEE <ms.jinhee.lee@gmail.com>
2022-03-30 19:52:13 +1100

    updates 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+var cities = [];
+
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+var formSumbitHandler = function(event){
+  event.preventDefault();
+  var city = cityInputEl.value.trim();
+  if(city){
+      getCityWeather(city);
+      get5Day(city);
+      cities.unshift({city});
+      cityInputEl.value = "";
+  } else{
+      alert("Please enter a City");
+  }
+  saveSearch();
+  pastSearch(city);
+}
+
+var saveSearch = function(){
+  localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+var getCityWeather = function(city){
+  var apiKey = "844421298d794574c100e3409cee0499"
+  var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  fetch(apiURL)
+  .then(function(response){
+      response.json().then(function(data){
+          displayWeather(data, city);
+      });
+  });
+};
 
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
