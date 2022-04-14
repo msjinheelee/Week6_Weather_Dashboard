a032773f19c06e7af8ac85cecf7d8ba2cf129de1
JINHEE LEE <ms.jinhee.lee@gmail.com>
2022-04-14 19:08:17 +1000

    css updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,20 @@ let weather = {
         const { speed } = data.wind; 
         console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerHTML = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerHTML = description;
         document.querySelector(".temp").innerHTML = temp + " °C";
         document.querySelector(".humidity").innerHTML = "humidity: " + humidity + "%";
         document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + " km/h";
+    },
+    search: function () {
+       this.fetchWeather(document.querySelector(".search-bar").value);
     }
 } ;
 
-
+    document.querySelector(".search button").addEventListener("click", function() {
+        weather.search();
+} );
 
 
 
